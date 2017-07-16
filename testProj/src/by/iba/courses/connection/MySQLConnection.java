b488e89885a7309d9d75cd1ffdfbf309913396c4
4kix <falko.ilya@gmail.com>
2017-07-16 15:06:55 +0300

    MySQLConnection class is now not singleton

--- testProj/src/by/iba/courses/connection/MySQLConnection.java
+++ testProj/src/by/iba/courses/connection/MySQLConnection.java
@@ -8,7 +8,6 @@ import by.iba.courses.dao.DAOException;
 
 public class MySQLConnection implements ConnectionFactory {
 	
-	private static MySQLConnection instance;
 	private Connection connection;
 	
 	private String user = "root";
@@ -16,16 +15,7 @@ public class MySQLConnection implements ConnectionFactory {
     private String url = "jdbc:mysql://localhost:3306/lapusha";	//URL адрес
     private String driver = "com.mysql.jdbc.Driver";			//Имя драйвера
 
-    
-    public static MySQLConnection getInstance() {
-    	if (instance == null) {
-    		instance = new MySQLConnection();
-    	}
-    	return instance;
-    }
-    
-    
-    private MySQLConnection() {
+    public MySQLConnection() {
         try {
             Class.forName(driver);	//Регистрируем драйвер
         } catch (ClassNotFoundException e) {
